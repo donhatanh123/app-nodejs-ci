6eb6a4d4f0a88b9b10491cfe84835672d070fa6f
donhatanh123 <44740110+donhatanh123@users.noreply.github.com>
2023-12-13 14:41:49 +0700

    change ip server to 'localhost'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
 app.get('/', function (req, res) {
     return res.send({ error: true, message: 'hello' })
 });
-var dbHost = '43.198.81.9';
+var dbHost = 'localhost';
 var dbPort = 3306;
 var dbUser = 'nodejs';
 var dbPassword = '123456a@';
